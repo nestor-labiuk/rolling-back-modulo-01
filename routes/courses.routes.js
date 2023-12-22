2f5fc1e087f13dfdf640d9407eee4a70c7ed7da7
nestor-labiuk <nestorlabiuk@gmail.com>
2023-12-22 13:57:16 -0300

    agrega validacion para base de datos vacia

--- routes/courses.routes.js
+++ routes/courses.routes.js
@@ -1,7 +1,12 @@
+const Course = require('../model/Course')
+
 const router = require('express').Router()
 
-router.get('/', (req, res) => {
-    res.json({ message: 'List of courses' })
+router.get('/', async(req, res) => {
+    const courses = await Course.find({})
+    if (courses.length !== 0) {
+        return res.json({ message: 'List of courses', courses })
+    }res.json({ message: 'List empty' })
 })
 router.get('/:id', (req, res) => {
     res.json({ message: 'Get course by id' })
