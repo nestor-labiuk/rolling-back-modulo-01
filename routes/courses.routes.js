ed577b61720edee4c5036dd78765f1f20310c14a
nestor-labiuk <nestorlabiuk@gmail.com>
2023-12-23 20:13:47 -0300

    agrega primeras validaciones con express-validator

--- routes/courses.routes.js
+++ routes/courses.routes.js
@@ -1,9 +1,19 @@
 const router = require('express').Router()
+const { body, validationResult } = require('express-validator')
 const { getAllCourses, getCourseById, createCourse, updateCourse, deleteCourse } = require('../controllers/courses.controllers')
 
 router.get('/', getAllCourses)
 router.get('/:id', getCourseById)
-router.post('/', createCourse)
+router.post('/',
+    body('name', 'Name is required').notEmpty(),
+    body('price', 'Price is required and must be a number').notEmpty().isNumeric(),
+    body('description', 'Description is required').notEmpty(),
+    (req, res, next) => {
+        const result = validationResult(req)
+        if (!result.isEmpty()) { return res.json({ errors: result.array() }) }
+        next()
+    },
+    createCourse)
 router.patch('/:id', updateCourse)
 router.delete('/:id', deleteCourse)
 
