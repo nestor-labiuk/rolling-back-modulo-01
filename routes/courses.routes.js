60c8975cbc6c9a536acc52fedd908e13fa8c3266
nestor-labiuk <nestorlabiuk@gmail.com>
2023-12-24 11:21:50 -0300

    captura y muestra el error capturado en el controlador

--- routes/courses.routes.js
+++ routes/courses.routes.js
@@ -6,14 +6,20 @@ router.get('/', getAllCourses)
 router.get('/:id', getCourseById)
 router.post('/',
     body('name', 'Name is required').notEmpty(),
-    // body('price', 'Price is required and must be a number').notEmpty().isNumeric(),
-    body('description', 'Description is required').notEmpty(),
+    body('price', 'Price is required').notEmpty(),
+    body('price', 'Price must be a number').isNumeric(),
+    // body('description', 'Description is required').notEmpty(),
     (req, res, next) => {
         const result = validationResult(req)
         if (!result.isEmpty()) { return res.json({ errors: result.array() }) }
         next()
     },
-    createCourse)
+    createCourse,
+    (err, req, res, next) => {
+        console.log('Error Capturado', err)
+        res.status(500)
+        res.json({ message: 'Internal server error ' })
+    })
 router.patch('/:id', updateCourse)
 router.delete('/:id', deleteCourse)
 
