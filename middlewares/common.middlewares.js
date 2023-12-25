04a484373f2e3069e57bce85ede3ef201dae6b4d
nestor-labiuk <nestorlabiuk@gmail.com>
2023-12-25 11:07:49 -0300

    crea las funciones requestValidation y errorMiddlewares en el  archivo commonMiddlewares para separar la logica del archivo de controladores y hacerlo mas legible

---
+++ middlewares/common.middlewares.js
@@ -0,0 +1,19 @@
+const { validationResult } = require('express-validator')
+
+const requestValidation = (req, res, next) => {
+    const result = validationResult(req)
+    if (!result.isEmpty()) { return res.json({ errors: result.array() }) }
+    next()
+}
+
+const errorMiddewares = (err, req, res, next) => {
+    console.log('Error Capturado', err)
+    res.status(500)
+    res.json({ message: 'Internal server error ' })
+    next()
+}
+
+module.exports = {
+    errorMiddewares,
+    requestValidation
+}
--- routes/courses.routes.js
+++ routes/courses.routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
-const { body, validationResult } = require('express-validator')
+const { body } = require('express-validator')
 const { getAllCourses, getCourseById, createCourse, updateCourse, deleteCourse } = require('../controllers/courses.controllers')
+const { errorMiddewares, requestValidation } = require('../middlewares/common.middlewares')
 
 router.get('/', getAllCourses)
 router.get('/:id', getCourseById)
@@ -8,18 +9,11 @@ router.post('/',
     body('name', 'Name is required').notEmpty(),
     body('price', 'Price is required').notEmpty(),
     body('price', 'Price must be a number').isNumeric(),
-    // body('description', 'Description is required').notEmpty(),
-    (req, res, next) => {
-        const result = validationResult(req)
-        if (!result.isEmpty()) { return res.json({ errors: result.array() }) }
-        next()
-    },
+    body('description', 'Description is required').notEmpty(),
+    requestValidation,
     createCourse,
-    (err, req, res, next) => {
-        console.log('Error Capturado', err)
-        res.status(500)
-        res.json({ message: 'Internal server error ' })
-    })
+    errorMiddewares
+)
 router.patch('/:id', updateCourse)
 router.delete('/:id', deleteCourse)
 
