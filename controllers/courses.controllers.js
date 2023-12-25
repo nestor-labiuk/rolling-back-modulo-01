7b3aed15a2107797eac9e14e8a6d2ca8992f24b4
nestor-labiuk <nestorlabiuk@gmail.com>
2023-12-25 12:59:55 -0300

    Agrega tryCatch a todos los controladores para manejar los errores y agrega validacion de id existente

--- controllers/courses.controllers.js
+++ controllers/courses.controllers.js
@@ -1,18 +1,28 @@
 const Course = require('../model/Course')
 
-const getAllCourses = async(req, res) => {
-    const courses = await Course.find({})
-    
-    if (courses.length !== 0) {
-        return res.json({ message: 'List of courses', courses })
-    }res.json({ message: 'List empty' })
+const getAllCourses = async(req, res, next) => {
+    try {
+        const courses = await Course.find({})
+        if (courses.length !== 0) {
+            return res.json({ message: 'List of courses', courses })
+        }res.json({ message: 'List empty' })
+    } catch (err) {
+        next(err)
+    }
 }
 
-const getCourseById = async (req, res) => {
-    const { id } = req.params
-
-    const course = await Course.findById(id)
-    res.json({ message: 'You got a course by id', course })
+const getCourseById = async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const course = await Course.findById(id)
+        if (!course) {
+            res.status(404) 
+            return res.json({ message: 'Course not found' })
+        }
+        res.json({ message: 'You got a course by id', course })
+    } catch (err) {
+        next(err)
+    }
 }
 
 const createCourse = async (req, res, next) => {
@@ -21,25 +31,37 @@ const createCourse = async (req, res, next) => {
         const course = new Course({ name, price, description })
         await course.save()
         res.json({ message: 'Create course', course })
-
     } catch (err) {
         next(err)
     }
 }
 
-const updateCourse = async (req, res) => {
-    const { id } = req.params
-    const { name, price, description } = req.body
-    const course = await Course.findByIdAndUpdate({ _id: id }, { name: name, price: price, description: description }, { new: true })
-
-    res.json({ message: 'Course update', course })
+const updateCourse = async (req, res, next) => {
+    try { const { id } = req.params
+        const { name, price, description } = req.body
+        const course = await Course.findByIdAndUpdate({ _id: id }, { name: name, price: price, description: description }, { new: true })
+        if (!course) {
+            res.status(404)
+            return res.json({ message: 'Course not found' })
+        }
+        res.json({ message: 'Course update', course })
+    } catch (err) {
+        next(err)
+    }
 }
 
-const deleteCourse = async (req, res) => {
-    const { id } = req.params
-    const course = await Course.findByIdAndDelete(id)
-
-    res.json({ message: 'Deleted course', course })
+const deleteCourse = async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const course = await Course.findByIdAndDelete(id)
+        if (!course) {
+            res.status(404)
+            return res.json({ message: 'Course not found' })
+        }
+        res.json({ message: 'Deleted course', course })
+    } catch (err) {
+        next(err)
+    }
 }
  
 module.exports = {
