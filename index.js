fbec33b646b45556d177a622d42e593236a38408
nestor-labiuk <nestorlabiuk@gmail.com>
2024-01-12 18:24:20 -0300

    agrega teacher routes aa index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan')
 require('dotenv').config()
 const mongoose = require('mongoose')
 const coursesRouter = require('./routes/courses.routes')
+const teachersRouter = require('./routes/teachers.routes')
 const { logger } = require('./loggers')
 
 const app = express()
@@ -12,6 +13,7 @@ app.use(express.json())
 app.use(morgan('dev'))
 
 app.use('/api/courses', coursesRouter)
+app.use('/api/teachers', teachersRouter)
 
 mongoose.connect(process.env.MONGO_CONNECTIONS)
     .then(() => logger.info({ message: 'Connected to data base' })) 
