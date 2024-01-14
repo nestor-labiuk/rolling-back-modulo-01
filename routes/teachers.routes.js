bf05dde78fd7be611f405f812f1ea6f03d9662da
nestor-labiuk <nestorlabiuk@gmail.com>
2024-01-14 11:39:41 -0300

    agrega rutas y middlewares  a teacher

--- routes/teachers.routes.js
+++ routes/teachers.routes.js
@@ -1,11 +1,40 @@
 const router = require('express').Router()
-const { getTeacher, createTeacher, deleteTeacher, getTeacherById, getTeacherByIdAndCourse } = require('../controllers/teachers.controllers')
+const { getTeachers, createTeacher, deleteTeacher, getTeacherById, getTeacherByIdAndCourse, updateTeacher } = require('../controllers/teachers.controllers')
+const { errorCatcher, validationMongoId } = require('../middlewares/common.middlewares')
 
-router.get('/', getTeacher)
-router.get('/:id', getTeacherById)
-router.get('/populate/:id', getTeacherByIdAndCourse)
-router.post('/', createTeacher)
-// router.patch('/:id',)
-router.delete('/:id', deleteTeacher)
+router.get('/',
+    getTeachers,
+    errorCatcher    
+)
+
+router.get('/:id',
+    validationMongoId,
+    getTeacherById,
+    errorCatcher    
+
+)
+
+router.get('/populate/:id',
+    validationMongoId,
+    getTeacherByIdAndCourse,
+    errorCatcher    
+)
+
+router.post('/',
+    createTeacher,
+    errorCatcher    
+)
+
+router.patch('/:id',
+    validationMongoId,
+    updateTeacher,
+    errorCatcher    
+)
+
+router.delete('/:id',
+    validationMongoId,
+    deleteTeacher,
+    errorCatcher    
+)
    
 module.exports = router
