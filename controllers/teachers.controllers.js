03db0e2db36a4bd16d842ac92f3c78ad6cb32f26
nestor-labiuk <nestorlabiuk@gmail.com>
2024-01-14 11:35:02 -0300

    completa los controladores de teachers y unufica mensajes de respuesta

--- controllers/teachers.controllers.js
+++ controllers/teachers.controllers.js
@@ -1,9 +1,16 @@
+const { logger, removedEntitiesLogger } = require('../loggers')
 const Teacher = require('../models/Teachers')
+const { errorResponse, succesResponse } = require('../utils/resonse.util')
 
-const getTeacher = async(req, res, next) => {
+const getTeachers = async(req, res, next) => {
     try {
         const teachers = await Teacher.find({})
-        res.json(teachers)
+        if (teachers.length === 0) {
+            logger.warn(errorResponse('Teacher list is empty', [] ))
+            return res.json(errorResponse('Teacher list is empty', [])) 
+        }
+        logger.info(succesResponse('You got the list of teachers', teachers))
+        res.json(succesResponse('You got the list of teachers', teachers ))
     } catch (error) {
         next(error)
     }
@@ -13,7 +20,13 @@ const getTeacherById = async(req, res, next) => {
     try {
         const { id } = req.params
         const teacher = await Teacher.findById(id)
-        res.json(teacher)
+        if (!teacher) {
+            res.status(404)
+            logger.warn(errorResponse('Teacher not found', []))
+            return res.json(errorResponse('Teacher not found', [] ))
+        }
+        logger.info(succesResponse('You got the teacher by id', teacher))
+        res.json(succesResponse('You got the teacher by id', teacher))
     } catch (error) {
         next(error)
     }
@@ -22,8 +35,14 @@ const getTeacherById = async(req, res, next) => {
 const getTeacherByIdAndCourse = async(req, res, next) => {
     try {
         const { id } = req.params
-        const teacher = await Teacher.findById(id).populate('course', { name: 1, _id: 0 })
-        res.json(teacher)
+        const teacher = await Teacher.findById(id).populate('course', { name: 1 })
+        if (!teacher) {
+            res.status(404)
+            logger.warn(errorResponse('Teacher not found', []))
+            return res.json(errorResponse('Teacher not found', [] ))
+        }
+        logger.info(succesResponse('You got the teacher and course by id', teacher))
+        res.json(succesResponse('You got the teacher and course by id', teacher))
     } catch (error) {
         next(error)
     }
@@ -34,7 +53,30 @@ const createTeacher = async (req, res, next) => {
         const { name, lastName, email, course } = req.body
         const teacher = new Teacher({ name, lastName, email, course })
         await teacher.save()
-        res.json(teacher)
+        logger.info(succesResponse('Teacher saved successfully', teacher))
+        res.json(succesResponse('Teacher saved successfully', teacher))
+    } catch (error) {
+        next(error)
+    }
+}
+
+const updateTeacher = async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const { name, lastName, email, course } = req.body
+        const currentTeacher = await Teacher.findById(id)
+        if (!currentTeacher) {
+            res.status(400)
+            logger.warn(errorResponse('Teacher not found', [] ))
+            return res.json(errorResponse('Teacher not found', [] ))
+        }
+        currentTeacher.name = name || currentTeacher.name
+        currentTeacher.lastName = lastName || currentTeacher.lastName
+        currentTeacher.email = email || currentTeacher.email
+        currentTeacher.course = course || currentTeacher.course
+        const teacher = await currentTeacher.save()
+        logger.info(succesResponse('Teacher update successfully', teacher))
+        res.json(succesResponse('Teacher update successfully', teacher))
     } catch (error) {
         next(error)
     }
@@ -44,16 +86,23 @@ const deleteTeacher = async (req, res, next) => {
     try {
         const { id } = req.params
         const teacher = await Teacher.findByIdAndDelete(id)
-        res.json({ deletedTeacher: teacher })
+        if (!teacher) {
+            res.status(404)
+            logger.warn(errorResponse('Teacher not found', [] ))
+            return res.json(errorResponse('Teacher not found', [] ))
+        }
+        removedEntitiesLogger.info(succesResponse('Deleted teacher', teacher))
+        res.json(succesResponse('Deleted teacher', teacher))
     } catch (error) {
         next(error)
     }
 }
 
 module.exports = {
-    getTeacher,
-    createTeacher,
+    getTeachers,
     getTeacherById,
-    deleteTeacher,
-    getTeacherByIdAndCourse
+    getTeacherByIdAndCourse,
+    createTeacher,
+    updateTeacher,
+    deleteTeacher
 }
